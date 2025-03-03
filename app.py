d105a7de04294d00502a8175462fce1c6e795555
ayeshaexe <ayeshaafarhan56@gmail.com>
2025-03-03 10:40:57 +0500

    first commit

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from PIL import Image
 import time
 
 st.header("PNG to JPG converter🖼️➡️📷")
-st.write("Easily converts you PNG file to JPG file allowing to manipulate and modify the image.🌆")
+st.write("Easily converts your PNG file to JPG file, allowing to manipulate and modify the image.🌆")
 def convert_png_to_jpg(image):
     # Convert the image to RGB mode (JPG doesn't support transparency)
     rgb_image = image.convert("RGB")
     return rgb_image
-image_uploader = st.file_uploader("📤 Upload your PNG file", type=["png"])
+image_uploader = st.file_uploader("Upload your PNG file 📤", type=["png"])
 if image_uploader  is not None:
     im = Image.open(image_uploader)
     st.image(im, caption="Uploaded PNG", use_container_width=True)
@@ -20,28 +20,24 @@ if image_uploader  is not None:
     resized_image = im.resize((new_width, new_height))
     st.image(resized_image, caption="Resized Image", use_container_width=True)
     st.header("🛠️🔄converting PNG to JPG")
-    if im != resized_image:
-        with st.status("Converting"):
-            time.sleep(7)
-            jpg = convert_png_to_jpg(resized_image)
-            st.success("Conversion complete!✅")
+    with st.status("Converting"):
+        time.sleep(7)
+        jpg = convert_png_to_jpg(resized_image)
+        st.success("Conversion complete!✅")
 
-        st.image(jpg, caption="Converted JPG and Resized Image", use_container_width=True)
+    st.image(jpg, caption="Converted JPG (Resized)", use_container_width=True)
 
-        buffer = io.BytesIO()
-        jpg.save(buffer, format="JPEG")
-        dataBytes = buffer.getvalue()
+    buffer = io.BytesIO()
+    jpg.save(buffer, format="JPEG")
+    dataBytes = buffer.getvalue()
 
-        st.download_button(
-        label="Download JPG File",
-        data=dataBytes,
-        file_name="convert_png_to_jpg",
-        mime="Image/jpeg"
+    st.download_button(
+    label="Download JPG File",
+    data=dataBytes,
+    file_name="converted_image.jpg",
+    mime="image/jpeg"
        
-        )
-    else:
-        print("Something went wrong!!!⚠️⚠️")
-
+    )
 st.markdown("---")
 st.markdown("© 2025 PNG to JPG converter. All rights reserved.")     
 
